99e111aa3066850d23fec45668b831ae38e9745d
ArvindChaturvedi1092 <Arvind.Chaturvedi@gopebble.com>
2025-03-06 15:47:37 +0530

    Updating initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -19,4 +19,5 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/rameswaram-breakfast-cafe/',  // Replace with your repository name
 }));
